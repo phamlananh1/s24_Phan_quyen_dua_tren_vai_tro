e278e005c6b0dbf701651303be1515af4b7f259f
phamlananh1 <phamlananh12011995@gmail.com>
2019-03-04 14:10:14 +0700

    file

---
+++ resources/views/layouts/page_admin.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lananh
+ * Date: 2019-03-04
+ * Time: 13:39
+ */
+echo "xin chao admin";
+
---
+++ resources/views/layouts/page_guest.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lananh
+ * Date: 2019-03-04
+ * Time: 11:53
+ */
+echo "xin chao guest";
+
